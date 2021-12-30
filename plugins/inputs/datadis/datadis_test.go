c2a5d6d2a0934c18cb0ec5b1a158a8e8ddc1152f
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2021-12-30 14:32:55 +0100

    test: add testing

--- plugins/inputs/datadis/datadis_test.go
+++ plugins/inputs/datadis/datadis_test.go
@@ -1,6 +1,7 @@
 package datadis
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,11 +10,20 @@ import (
 	"github.com/influxdata/telegraf/config"
 )
 
-func NewServer() *httptest.Server {
-	mux := http.NewServeMux()
+func TestFetchConsumption(t *testing.T) {
+	endDate := time.Now().Format("2006/01/02")
+	startDate := time.Now().Add(-24 * time.Hour).Format("2006/01/02")
 
-	mux.HandleFunc("/api-private/api/get-consumption-data", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte(`[ {
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		if query.Get("startDate") != startDate {
+			t.Fatalf("expected: %q, got: %q", startDate, query.Get("startDate"))
+		}
+		if query.Get("endDate") != endDate {
+			t.Fatalf("expected: %q, got: %q", endDate, query.Get("endDate"))
+		}
+
+		fmt.Fprint(rw, `[ {
 			"cups" : "1234",
 			"date" : "2021/12/28",
 			"time" : "01:00",
@@ -25,40 +35,60 @@ func NewServer() *httptest.Server {
 			"time" : "24:00",
 			"consumptionKWh" : 0.117,
 			"obtainMethod" : "Real"
-		  } ]`))
-	})
-
-	ts := httptest.NewServer(mux)
-
-	return ts
-}
-
-func TestFetchAll(t *testing.T) {
-	ts := NewServer()
+		  } ]`)
+	}))
 	defer ts.Close()
 
-	d := Datadis{
-		Username:        "u",
-		Password:        "p",
-		token:           "t",
-		MeasurementType: HOURLY,
-		DateDuration:    config.Duration(24 * time.Hour),
-		url:             ts.URL,
-		Supplies:        []Supply{{Cups: "C", DistributorCode: "2", PointType: 0}},
-		httpClient:      ts.Client(),
-	}
+	t.Run("Should use time range", func(t *testing.T) {
+		d := Datadis{
+			url:        ts.URL,
+			httpClient: ts.Client(),
+			StartDate:  startDate,
+			EndDate:    endDate,
+		}
 
-	data, err := fetchConsumption(d, d.Supplies[0])
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
+		_, err := fetchConsumption(d, Supply{})
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if len(data) != 2 {
-		t.Fatalf("wanted: 2 got %d", len(data))
-	}
+	})
+	t.Run("Should calculate time range", func(t *testing.T) {
+		d := Datadis{
+			url:          ts.URL,
+			httpClient:   ts.Client(),
+			DateDuration: config.Duration(24 * time.Hour),
+		}
 
-	_, err = data[1].timestamp()
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
+		_, err := fetchConsumption(d, Supply{})
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("Should parse response", func(t *testing.T) {
+		d := Datadis{
+			url:        ts.URL,
+			httpClient: ts.Client(),
+			StartDate:  startDate,
+			EndDate:    endDate,
+		}
+
+		got, err := fetchConsumption(d, Supply{})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(got) != 2 {
+			t.Fatalf("expected: %d, got: %d", 2, len(got))
+		}
+
+		timestamp, err := got[1].timestamp()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if timestamp.Unix() != 1640649600 {
+			t.Fatalf("expected: %d, got: %d", 1640649600, timestamp.Unix())
+		}
+	})
 }
